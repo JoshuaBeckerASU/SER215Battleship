e876f07f7c9d6e8d376db7f19adb245336e9aca1
crazyivan85204 <crazyivan85204@yahoo.com>
2015-11-10 10:17:35 -0700

    update
    user login window

--- Battleship2.0/Source/UserLogin.java
+++ Battleship2.0/Source/UserLogin.java
@@ -14,24 +14,33 @@ import javax.imageio.*;
 import java.awt.image.*;
 import java.io.*;
 
-public class UserLogin
+public class UserLogin extends JFrame
 {
-   private JFrame m_UserLoginFrame;
+   private JFrame m_UserLoginFrame, m_UserRegFrame;
    private int m_ScreenWidth, m_ScreenHeight;
    private JButton m_Login, m_Register, m_NewUser, m_Cancel, m_Exit; //m_Login button to login as existing user, m_Register button to register new user, m_NewUser button to bring up new user registration
    private JPanel m_LabelText_Existing_P, m_Buttons_Existing_P, m_LabelText_New_P, m_Buttons_New_P;
    private JLabel m_UserName_L, m_Password_L, m_Results_L;
+   private JTextField m_UserName_T, m_Password_T;
    private String m_UserName_S, m_Password_S, m_LoginResult_S, m_RegResult_S;
 
    public static void main(String[] args)
    {
-      
+      new UserLogin();
+   }
+   
+   public UserLogin()
+   {
+      createComponents();
+      buildComponentsLog();
+      addElements();
+      m_UserLoginFrame.setVisible(true);
    }
    
    //check if username and password are correct
    public boolean loginSuccess()
    {
-      boolean isLoggedIn;
+      boolean isLoggedIn = false;
       int resultCode;
       
       UserRegistration logUser = new UserRegistration(m_UserName_S, m_Password_S);
@@ -59,7 +68,7 @@ public class UserLogin
    //check if user registered correctly
    public boolean registrationSuccess()
    {
-      boolean isLoggedIn;
+      boolean isLoggedIn = false;
       int resultCode;
       
       UserRegistration userReg = new UserRegistration(m_UserName_S, m_Password_S);
@@ -82,5 +91,60 @@ public class UserLogin
       }
       
       return isLoggedIn;
+   }
+   
+   public void createComponents()
+   {
+      GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();// geting size of screen
+		m_ScreenWidth = gd.getDisplayMode().getWidth();
+		m_ScreenHeight = gd.getDisplayMode().getHeight();
+      
+      m_UserLoginFrame = new JFrame("User Login");
+      m_UserRegFrame = new JFrame("Create User");
+      
+      m_Login = new JButton("Sign in");
+      m_NewUser = new JButton("New User");
+      m_Exit = new JButton("Exit");
+      m_Cancel = new JButton("Cancel");
+      m_Register = new JButton("Register");
+      
+      
+      m_UserName_L = new JLabel("Username:");
+      m_Password_L = new JLabel("Password:");
+      m_Results_L = new JLabel("");
+      
+      m_UserName_T = new JTextField(40);
+      m_Password_T = new JTextField(40);
+      
+      m_LabelText_Existing_P = new JPanel();
+      m_Buttons_Existing_P = new JPanel();
+      m_LabelText_New_P = new JPanel();
+      m_Buttons_New_P = new JPanel();
+   }
+   
+   public void buildComponentsLog()
+   {
+      m_UserLoginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      m_UserLoginFrame.setSize(new Dimension(m_ScreenWidth,m_ScreenHeight));
+      
+      m_Login.setMargin(new Insets(0,0,0,0));
+      m_NewUser.setMargin(new Insets(0,0,0,0));
+      m_Exit.setMargin(new Insets(0,0,0,0));
+   }
+   
+   public void addElements()
+   {
+      m_LabelText_Existing_P.add(m_UserName_L);
+      m_LabelText_Existing_P.add(m_UserName_T);
+      m_LabelText_Existing_P.add(m_Password_L);
+      m_LabelText_Existing_P.add(m_Password_T);
+      m_LabelText_Existing_P.add(m_Results_L);
+      
+      m_Buttons_Existing_P.add(m_Login);
+      m_Buttons_Existing_P.add(m_NewUser);
+      m_Buttons_Existing_P.add(m_Exit);
+      
+      m_UserLoginFrame.add(m_LabelText_Existing_P);
+      m_UserLoginFrame.add(m_Buttons_Existing_P);
    }
 }
