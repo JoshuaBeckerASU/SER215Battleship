2ae4433d4d159c15ef088bad685ef180cb0dfc81
JoshuaBeckerASU <jdbecke3@asu.edu>
2015-11-13 17:06:42 -0700

    MultiPlayer

---
+++ Battleship2.0/Source/MultiPlayerGame.java
@@ -0,0 +1,20 @@
+/********************** 
+Name: MultiPlayerGame Object
+Author: Joshua Becker
+Create On: 10/26/15
+Contributors:
+***********************/
+import javax.swing.*;
+import java.util.*;
+
+
+public class MultiPlayerGame extends Game
+{
+    private GameServer m_Server;
+	MultiPlayerGame(int difficulty, LobbySlot slots[], LoadAssets assets)
+	{
+        super(difficulty, slots, assets);
+        m_Server = new GameServer();
+        
+	}
+}
