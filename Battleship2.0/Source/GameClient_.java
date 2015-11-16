cb4163ded3b90256ae670ff1a3a4e3c1b4cf25ad
atshinn1 <atshinn1@gmail.com>
2015-11-16 13:52:21 -0700

    GameClient Chat
    two private classes for chat have been added but not implemented

--- Battleship2.0/Source/GameClient_.java
+++ Battleship2.0/Source/GameClient_.java
@@ -49,4 +49,56 @@ public class GameClient_
   {
       return toServer;
   }
+
+  private class ChatService implements Runnable{
+        private ObjectInputStream fromServer;
+        private ObjectOutputStream toServer;
+        
+        public ChatService(ObjectInputStream ois,ObjectOutputStream oos){
+            fromServer=ois;
+            toServer=oos;
+
+        }
+
+        public void run(){
+            MessageListener chatListener=new MessageListener(fromServer);
+            
+
+            Thread listener=new Thread(chatListener);
+            
+            while(listener.isAlive()){
+                if(!(listener.isAlive())){
+                    String message=chatListener.getMessage();
+                    toServer.writeObject(new String(message));
+                    listener.start();
+                }
+                
+        }
+        public ObjectInputStream getChatInputStream(){
+          return fromServer;
+        }
+        
+        public ObjectOutputStream getChatOutputStream(){
+          return toServer;
+        }
+
+        private class MessageListener implements Runnable{
+
+            private ObjectInputStream fromServer;
+            private String message;
+
+            public MessageListener(ObjectInputStream ois){
+                fromServer=ois;
+                message="";
+            }
+
+            public getMessage(){
+                return message;
+            }
+
+            public void run(){
+                message=(String)fromServer.readObject();
+            }
+        }
+    }
 }
